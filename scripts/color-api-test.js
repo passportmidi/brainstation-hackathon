ae8ea3b994bb0f3599df2c8fac2014a96444673a
Sydney Putnam <sydney.putnam@proton.me>
2025-02-06 13:06:14 -0400

    move test js to test file and link to test html instead

---
+++ scripts/color-api-test.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("colorForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const colorHex = e.target.colorPicker.value.slice(1);
+
+    console.log(`https://www.thecolorapi.com/scheme?hex=${colorHex}&count=3`);
+
+    const resp = await axios.get(
+      `https://www.thecolorapi.com/scheme?hex=${colorHex}&count=3`
+    );
+    console.log(resp);
+    for (let i = 0; i < 3; i++) {
+      console.log(resp.data.colors[i].hex.value);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
